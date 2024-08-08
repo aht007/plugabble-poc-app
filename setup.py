86f373d0af3d8b1d3789367cacb06c0a53e963c2
Mohammad Ahtasham ul Hassan <ahthassan74@gmail.com>
2024-08-08 20:04:50 +0500

    feat: add basic repo structure

---
+++ setup.py
@@ -0,0 +1,14 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    name="plugabble-poc-app",
+    version="0.1",
+    description="A Django app to demonstrate how to create a pluggable app",
+    url="",
+    author="ahtasham",
+    author_email="ahthassan74@gmail.com",
+    license="MIT",
+    packages=["pluggable_poc_app"],
+    zip_safe=False,
+)
